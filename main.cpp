176069b2e449de7471c8aa314e5b59b41c59e1aa
Tang Piseth <tangpiseth43@gmail.com>
2024-08-18 16:36:23 +0930

    Test GS

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-#include <sstream>
+#include <vector>
+#include <algorithm>
 
-
+//
+std::vector<int> digits_transform_alloc(const std::string& s)
+{
+    std::vector<int> result(s.size());
+    std::transform(
+        s.begin(), s.end(),
+        result.begin(), 
+        [](char ch) {return ch - '0';}
+        );
+    return result;
+}
 
 // Integer addition 
+int GRADE_SCHOOL_INTEGER_SUBTRACTION(std::vector<int> i1, std::vector<int> i2, int B){
 
+  return 0;
+}
 // Working with base 2
-int GRADE_SCHOOL_INTEGER_ADDITION(int i1, int i2, int B){
+int GRADE_SCHOOL_INTEGER_ADDITION(std::vector<int> i1, std::vector<int> i2, int B){
 
   return 0;
 }
@@ -15,41 +29,42 @@ int GRADE_SCHOOL_INTEGER_ADDITION(int i1, int i2, int B){
 
 
 // Karatsuba Algorithm for Integer Multiplication
-int KARATSUBA_ALGORITHM(int i1, int i2, int B){
+int KARATSUBA_ALGORITHM(std::vector<int> i1, std::vector<int> i2, int B){
   return 1;
 }
 
 
 // Integer division (Post-graduate only)
-int INTEGER_DIVISION(int i1, int i2, int B){
+int INTEGER_DIVISION(std::vector<int> i1, std::vector<int> i2, int B){
   return 0;
 }
 
 
 int main(int argc, char* argv[]){
   
-  // Declare and initialize the variables extracted from the command-line argument  
-  int I1 = std::atoi(argv[1]);
-  int I2 = std::atoi(argv[2]);
-  int B = std::atoi(argv[3]);
+    // Declare and initialize the variables extracted from the command-line argument  
+    std::string pre_I1 = argv[1];
+    std::string pre_I2 = argv[2];
+    std::string pre_B = argv[3];
+    // std::cout << pre_I1 << std::endl;
 
+    // Extract each digit into a list of integers.
+    std::vector<int> I1 = digits_transform_alloc(pre_I1);
+    std::vector<int> I2 = digits_transform_alloc(pre_I2);
+    std::vector<int> B_vec = digits_transform_alloc(pre_B);
 
+    int B = B_vec[0];
   
-  // Output the extracted values
-  // std::cout << "I1: " << I1 << std::endl;
-  // std::cout << "I2: " << I2 << std::endl;
-  // std::cout << "B: " << B << std::endl;
-  
-  int res_addition = GRADE_SCHOOL_INTEGER_ADDITION(I1, I2, B);
-  int res_multiplication = KARATSUBA_ALGORITHM(I1, I2, B);
-  int res_division = INTEGER_DIVISION(I1, I2, B);
+    int res_addition = GRADE_SCHOOL_INTEGER_ADDITION(I1, I2, B);
+    int res_multiplication = KARATSUBA_ALGORITHM(I1, I2, B);
+    int res_division = INTEGER_DIVISION(I1, I2, B);
 
 
-  std::cout << res_addition 
-            << " "
-            <<  res_multiplication 
-            << " " 
-            << res_division;
+    std::cout << res_addition 
+              << " "
+              <<  res_multiplication 
+              << " " 
+              << res_division;
 
   return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,21 +1,62 @@
+#include <algorithm>
 #include <sstream>
 #include <iostream>
-
+#include <vector>
 #include <string>
 
 
+std::vector<int> digits_transform_alloc(const std::string& s)
+{
+    std::vector<int> result(s.size());
+    std::transform(
+        s.begin(), s.end(),
+        result.begin(), 
+        [](char ch) {return ch - '0';}
+        );
+    return result;
+}
+
+void print_digits(const std::vector<int>& result){
+    u_int size = result.size();
+    std::cout << "<";
+    for (int i=0; i<size; i++){
+        if (i == size -1) std::cout << result[i];
+        else std::cout << result[i] << ",";   
+    }
+    std::cout << ">" << std::endl;
+}
+
 
 int main(int argc, char *argv[]){
-    std::string sentence = "I would like to have a talk with my friend.";
-    char delimiter = ' ';
+    // std::string sentence = "I would like to have a talk with my friend.";
+    // char delimiter = ' ';
 
-    std::istringstream iss(sentence);
-    std::string word = "";
+    // std::istringstream iss(sentence);
+    // std::string word = "";
 
-    while (iss >> word){
-        std::cout << word << std::endl;
-        
-    }
+    // while (iss >> word){
+    //     std::cout << word << std::endl;
+    // }
+
+    // int I1 = atoi(argv[1]);
+    // int I2 = atoi(argv[2]);
+    // int B = atoi(argv[3]);
+    std::string pre_I1 = argv[1];
+    std::string pre_I2 = argv[2];
+    std::string pre_B = argv[3];
+    // std::cout << pre_I1 << std::endl;
+
+    // Extract each digit into a list of integers.
+    std::vector<int> I1 = digits_transform_alloc(pre_I1);
+    std::vector<int> I2 = digits_transform_alloc(pre_I2);
+    std::vector<int> B = digits_transform_alloc(pre_B);
+
+    print_digits(I1);    
+    print_digits(I2);    
+    print_digits(B);    
+    
     
     return 0;
 }
+
+// ./run.sh 111111101111011111110110110001000101111000011100111
