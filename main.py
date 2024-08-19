9444d03b3a2ac1619ca395bc98175e48ecb4ed92
Tang Piseth <tangpiseth43@gmail.com>
2024-08-19 11:01:50 +0930

    Using Python

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def digits_transform_output(v):
     result = ''.join(map(str, v))
     return result.lstrip('0') or '0'
 
+def get_input():
+    # Prompt the user to enter three integers separated by spaces
+    # user_input = input("Enter three non-negative integers (I1 I2 B), separated by spaces: ")
+    user_input = input()
+    # Split the input string into three parts
+    I1_str, I2_str, B_str = user_input.split()
+    
+    # Convert the strings to integers
+    I1 = int(I1_str)
+    I2 = int(I2_str)
+    B = int(B_str)
+    
+    return I1, I2, B
+
+
 def align(I1, I2):
     len_diff = len(I1) - len(I2)
     if len_diff > 0:
@@ -86,25 +101,45 @@ def main():
     # pre_I1 = sys.argv[1]
     # pre_I2 = sys.argv[2]
     # pre_B = sys.argv[3]
-    pre_I1 = str(240540143205242311502340531412055204150332)
-    pre_I2 = str(2401150215333414545353530050)
-    pre_B = str(6)
+    # Get 3 integers from the user 
+    pre_I1, pre_I2, pre_B = get_input()
 
-    I1 = digits_transform_alloc(pre_I1)
-    I2 = digits_transform_alloc(pre_I2)
-    B = int(pre_B)
+    # Transforms each input into a vector of integers
+    I1 = digits_transform_alloc(str(pre_I1))
+    I2 = digits_transform_alloc(str(pre_I2))
+    B = int(str(pre_B))
+    
+    # print(f"{I1}, {I2}, {B}")
 
     addition = GRADE_SCHOOL_INTEGER_ADDITION(I1, I2, B)
     multiplication = KARATSUBA_ALGORITHM(I1, I2, B)
     division = DIVISION_ALGORITHM(I1, I2, B)
+    # subtraction = GRADE_SCHOOL_INTEGER_SUBTRACTION(I1, I2, B)
+    
     
     add_result = digits_transform_output(addition)
+    # sub_result = digits_transform_output(subtraction)
     mult_result = digits_transform_output(multiplication)
     div_result = digits_transform_output(division)
-    # print("Addition:", digits_transform_output(addition))
+    
+    # print(f"Addition: {add_result}")
+    # print(f"Subtraction: {sub_result}")
+    # print(f"Multiplication: {mult_result}")
     # print("Multiplication:", digits_transform_output(multiplication))
     # print("Division:", digits_transform_output(division))
     print(f"{add_result} {mult_result} {div_result}")
     
+    
+    
+    # print("---------------------------------FULL TEST--------------------------------------------")
+    # test_add = 34303100302104324243311110103331002244444311310243102223032232133423342010144440202004040320213 
+    # test_mult = 23132020143304230203120440421433140433104433224312424214100403111220004124440112331143134010413414200123334222243310102330 
+    # test_div = 103420003131314212110244422124214444340431341001134002033013412440310
+    # if (str(test_add) == add_result and 
+    #     str(test_mult) == mult_result):
+        
+    #     print("Hellalujah!")
+    # else: 
+    #     print("=====================Something is wrong!===================")
 if __name__ == "__main__":
     main()
